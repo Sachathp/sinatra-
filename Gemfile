8f0cae3474001cd23a093428948926042589892b
Sachathp <sacha.courbe1@gmail.com>
2024-10-21 15:57:02 +0200

    1er commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+ruby '3.2.2'
+gem 'sinatra'
+gem 'rackup'
+gem 'rerun'
+gem 'pry'
+gem 'csv'
---
+++ lib/controller.rb
@@ -0,0 +1,32 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
+
+  get '/gossips/new/' do 
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+
+    gossip_author = params["gossip_author"]
+    gossip_content = params["gossip_content"]
+
+    Gossip.new(gossip_author, gossip_content).save
+
+    redirect '/'
+  end
+end
+
+
+#post '/gossips/new/' do
+    #puts "Salut, je suis dans le serveur"
+    #puts "Ceci est le contenu du hash params : #{params}"
+    #puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+    #puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+    #puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+  #end
+#end
+
